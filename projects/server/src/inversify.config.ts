9e15ce0aa33d6b1853a593de43c1d9905866a968
Johan Bouduin <johan@der-e-coach.de>
2019-04-02 11:22:43 +0530

    implemented /hello
    just a kind of dummy call, routed and returning a simple JSON

--- projects/server/src/inversify.config.ts
+++ projects/server/src/inversify.config.ts
@@ -1,10 +1,15 @@
 import { Container } from 'inversify';
 
+import CONTROLLERTYPES from './controllers/controller.types';
 import SERVICETYPES from './services/service.types';
 
+import { IHomeController, HomeController } from './controllers';
 import { IRouteService, RouteService } from './services';
 
 const container = new Container();
+
+// controllers
+container.bind<IHomeController>(CONTROLLERTYPES.HomeController).to(HomeController);
 
 // services
 container.bind<IRouteService>(SERVICETYPES.RouteService).to(RouteService);
--- projects/server/src/services/route.service.ts
+++ projects/server/src/services/route.service.ts
@@ -2,6 +2,11 @@ import { Application, Request, Response } from 'express';
 import { injectable, inject } from 'inversify';
 import 'reflect-metadata';
 
+import CONTROLLERTYPES from '../controllers/controller.types';
+
+import { IHomeController } from 'controllers';
+
+
 export interface IRouteService {
   setRoutes(app: Application): void;
 }
@@ -10,9 +15,16 @@ export interface IRouteService {
 export class RouteService implements IRouteService {
 
   // constructor
-  public constructor() { }
+  public constructor(
+    @inject(CONTROLLERTYPES.HomeController) private homeController: IHomeController
+  ) { }
 
   public setRoutes(app: Application): void {
+
+    app.route('/hello')
+      .all((_request: Request, response: Response) => {
+        this.homeController.HelloWorld(_request, response);
+      });
     app.route('*')
       .all((_request: Request, response: Response) => {
         response.sendStatus(404);
