48bc5273b765fca5adae7514ba03c3a330698abb
Johan Bouduin <johan@der-e-coach.de>
2019-10-31 08:49:58 +0100

    Removed all string values that referred to the app from which this template was derived

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit{
-  title = 'sample app';
+  title = 'Template';
   greeting: string;
 
   ngAfterViewInit(): void {
