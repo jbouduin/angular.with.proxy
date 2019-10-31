21b907541552b49e84539b49a9c28c2c515982ee
Johan Bouduin <johan@der-e-coach.de>
2019-10-31 08:56:51 +0100

    Updated tslint.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   title = 'Template';
   greeting: string;
 
