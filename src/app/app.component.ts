8ef99d9724dd18d7528319e4a100512de0b164d8
Johan Bouduin <johan@der-e-coach.de>
2020-04-15 10:47:37 +0200

    make things work again before updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements AfterViewInit {
   greeting: string;
 
   ngAfterViewInit(): void {
-    this.http.get('/api/hello').subscribe(data => this.greeting = data.message);
+    this.http.get('/api/hello').subscribe( (data: any) => this.greeting = data.message);
   }
 
   constructor(private http: HttpClient) { }
